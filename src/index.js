b55e0ebfd1d9e8b7a77f06bbc05f59fdce2b856c
david <davedox@protonmail.com>
2021-09-22 19:39:51 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+console.log('helo');
+
+let data = [
+    // week 1
+    [
+        // first row of week 1
+        {
+            teamLeft: {
+                name: '',
+                score: '',
+            },
+            teamRight: {
+                name: '',
+                score: '',
+            },
+        },
+        // second row of week 1
+        {
+            teamLeft: {},
+            teamRight: {},
+        }
+    ],
+    // week 2
+    [
+        '...'
+    ]
+]
+
